f554d739b2c838cda19bfd725a78aaa202258920
joery van santen <joeryvansanten@kpnmail.nl>
2022-11-21 19:24:17 +0100

    added documentatie

--- Engine/Control/IInputHelper.cs
+++ Engine/Control/IInputHelper.cs
@@ -10,9 +10,28 @@ namespace Engine.Control
 {
     public interface IInputHelper
     {
+        /// <summary>
+        /// updates the states
+        /// </summary>
+        /// <param name="gametime">gives you timespan safety</param>
         public void UpdateTheStates(GameTime gametime);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key">the Key to check</param>
+        /// <returns>true is key is down otherwise false</returns>
         public bool IsKeyDown(Keys key);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key">The Key to check</param>
+        /// <returns>true if key is now down and last frame up</returns>
         public bool IsKeyPressed(Keys key);
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="key">The Key to check</param>
+        /// <returns>true if key is up and last frame down</returns>
         public bool IsKeyReleased(Keys key);
 
         public bool IsAnyKeyDown(Keys[] keys);
