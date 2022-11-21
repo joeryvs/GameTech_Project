e78da8f6656bb9c5c9e7d1cbd891a529b951b554
joery van santen <joeryvansanten@kpnmail.nl>
2022-11-21 17:44:34 +0100

    final stuff

---
+++ Engine/GameStates/GameStateManager.cs
@@ -0,0 +1,4 @@
+﻿namespace Engine.GameStates
+{
+    public class GameStateManager { }
+}
---
+++ WizardWarsGame/Levels/GameLevel.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WizardWarsGame.Levels
+{
+    class GameLevel : Engine.ObjectList.GameObjectList
+    {
+    }
+}
