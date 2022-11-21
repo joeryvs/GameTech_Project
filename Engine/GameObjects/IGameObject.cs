4a9aae1183ac8675802948aa4df20b668601cd93
joery van santen <joeryvansanten@kpnmail.nl>
2022-11-21 18:59:05 +0100

    added documentatie

--- Engine/GameObjects/IGameObject.cs
+++ Engine/GameObjects/IGameObject.cs
@@ -4,19 +4,41 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Microsoft.Xna.Framework;
+using Engine.Control;
+using Microsoft.Xna.Framework.Graphics;
 
 namespace Engine.GameObjects
 {
     internal interface IGameObject
     {
-        public void Update(Microsoft.Xna.Framework.GameTime gametime);
+        /// <summary>
+        /// updates the position and or sprite of the object
+        /// </summary>
+        /// <param name="gametime">amount of time passed since last call</param>
+        public void Update(GameTime gametime);
 
-        public void HandleInput( Engine.Control.InputHelper inputHelper );
+        /// <summary>
+        /// Handles the input
+        /// </summary>
+        /// <param name="inputHelper">input helper used for IO methoden</param>
+        public void HandleInput(InputHelper inputHelper );
 
-        public void Draw( Microsoft.Xna.Framework.Graphics.SpriteBatch spritebatch ,Microsoft.Xna.Framework.GameTime gametime );
 
+        /// <summary>
+        /// Draws the object at the GlobalPosition
+        /// </summary>
+        /// <param name="spritebatch">spritebatch needed to draw</param>
+        /// <param name="gametime">It is usually provided</param>
+        public void Draw(SpriteBatch spritebatch , GameTime gametime );
+
+        /// <summary>
+        /// mild reset
+        /// </summary>
         public void Reset();
 
+        /// <summary>
+        /// complete reset to the initial state
+        /// </summary>
         public void FullReset();
 
 
