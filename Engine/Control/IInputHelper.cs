bd1df95421fa2dda535edce164bb9bd11a4c84e2
joery van santen <joeryvansanten@kpnmail.nl>
2022-11-21 19:20:58 +0100

    updated inputHelper

--- Engine/Control/IInputHelper.cs
+++ Engine/Control/IInputHelper.cs
@@ -3,10 +3,32 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework.Input;
+using Microsoft.Xna.Framework;
 
 namespace Engine.Control
 {
     public interface IInputHelper
     {
+        public void UpdateTheStates(GameTime gametime);
+        public bool IsKeyDown(Keys key);
+        public bool IsKeyPressed(Keys key);
+        public bool IsKeyReleased(Keys key);
+
+        public bool IsAnyKeyDown(Keys[] keys);
+        public bool IsAnyKeyDown(List<Keys> keys);
+
+        public bool IsAnyKeyPressed(Keys[] keys);
+        public bool IsAnyKeyPressed(List<Keys> keys);
+
+        public bool IsAnyKeyReleased(List<Keys> keys);
+        public bool IsAnyKeyReleased(Keys[] keys);
+
+        public Vector2 CurrentMousePosition { get; }
+
+        public Vector2 GameWorldMousePosition { get; }
+
+        public bool LeftMouseButtonPressed { get; }
+
     }
 }
--- Engine/Control/InputHelper.cs
+++ Engine/Control/InputHelper.cs
@@ -1,4 +1,111 @@
-﻿namespace Engine.Control
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Input;
+using System.Collections.Generic;
+
+namespace Engine.Control
 {
-    public class InputHelper : IInputHelper { }
+    public class InputHelper : IInputHelper
+    {
+
+
+
+        public readonly ExtendedGame game;
+        protected System.TimeSpan ts;
+        MouseState currentMouseState, previousMouseState;
+        KeyboardState currentKeyboardState, previosKeyboardState;
+
+        public InputHelper(ExtendedGame game)
+        {
+            this.game = game;
+        }
+        public void UpdateTheStates(GameTime gametime)
+        {
+            System.TimeSpan temp = gametime.TotalGameTime;
+            if (ts == temp)
+            {
+                return;
+            }
+            previosKeyboardState = currentKeyboardState;
+            previousMouseState = currentMouseState;
+
+            // update vals
+            currentMouseState = Mouse.GetState();
+            currentKeyboardState = Keyboard.GetState();
+
+            ts = temp;
+            return;
+        }
+        public Vector2 CurrentMousePosition => currentMouseState.Position.ToVector2();
+
+        public Vector2 GameWorldMousePosition => throw new System.NotImplementedException();
+
+        public bool LeftMouseButtonPressed => currentMouseState.LeftButton == ButtonState.Pressed && previousMouseState.LeftButton == ButtonState.Released;
+
+        public bool IsKeyDown(Keys key)
+        {
+            return currentKeyboardState.IsKeyDown(key);
+        }
+
+        public bool IsKeyPressed(Keys key)
+        {
+            return currentKeyboardState.IsKeyDown(key) && previosKeyboardState.IsKeyUp(key);
+        }
+
+        public bool IsKeyReleased(Keys key)
+        {
+            return currentKeyboardState.IsKeyUp(key) && previosKeyboardState.IsKeyDown(key);
+        }
+
+        public bool IsAnyKeyDown(Keys[] keys)
+        {
+            foreach (Keys keys1 in keys)
+            {
+                if (IsKeyDown(keys1))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsAnyKeyDown(List<Keys> keys)
+        {
+            return IsAnyKeyDown(keys.ToArray());
+        }
+
+        public bool IsAnyKeyPressed(Keys[] keys)
+        {
+            foreach (Keys item in keys)
+            {
+                if (IsKeyPressed(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public bool IsAnyKeyPressed(List<Keys> keys)
+        {
+            return IsAnyKeyPressed(keys.ToArray());
+        }
+
+        public bool IsAnyKeyReleased(List<Keys> keys)
+        {
+            return IsAnyKeyReleased(keys.ToArray());
+        }
+
+        public bool IsAnyKeyReleased(Keys[] keys)
+        {
+            foreach (Keys item in keys)
+            {
+                if (IsKeyReleased(item))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+    }
 }
