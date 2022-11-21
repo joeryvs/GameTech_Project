7c341b02300b195d1180a99fe478feaa8dfce52c
joery van santen <joeryvansanten@kpnmail.nl>
2022-11-21 18:46:27 +0100

    implemented IgameObject

--- Engine/GameObjects/GameObject.cs
+++ Engine/GameObjects/GameObject.cs
@@ -1,4 +1,50 @@
-﻿namespace Engine.GameObjects
+﻿using Engine.Control;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Engine.GameObjects
 {
-    public abstract class GameObject : IGameObject { }
+    public abstract class GameObject : IGameObject
+    {
+
+        protected Vector2 Velocity { get; set; }
+        private Vector2 localPosition = Vector2.Zero;
+        public Vector2 StartPosition { get; set; }
+        public Vector2 LocalPosition { get => localPosition; set => localPosition = value; }
+
+        protected GameObject()
+        {
+            Velocity = Vector2.Zero;
+        }
+
+        public GameObject(Vector2 startPos) : this()
+        {
+            StartPosition = startPos;
+        }
+
+        public virtual void Draw(SpriteBatch spritebatch, GameTime gametime)
+        {
+            // empty
+        }
+
+        public virtual void FullReset()
+        {
+            throw new System.NotImplementedException();
+        }
+
+        public virtual void HandleInput(InputHelper inputHelper)
+        {
+            // empty
+        }
+
+        public virtual void Reset()
+        {
+            LocalPosition = StartPosition;
+        }
+
+        public virtual void Update(GameTime gametime)
+        {
+            LocalPosition += Velocity * (float)gametime.ElapsedGameTime.TotalSeconds;
+        }
+    }
 }
--- Engine/GameObjects/IGameObject.cs
+++ Engine/GameObjects/IGameObject.cs
@@ -3,10 +3,23 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.Xna.Framework;
 
 namespace Engine.GameObjects
 {
     internal interface IGameObject
     {
+        public void Update(Microsoft.Xna.Framework.GameTime gametime);
+
+        public void HandleInput( Engine.Control.InputHelper inputHelper );
+
+        public void Draw( Microsoft.Xna.Framework.Graphics.SpriteBatch spritebatch ,Microsoft.Xna.Framework.GameTime gametime );
+
+        public void Reset();
+
+        public void FullReset();
+
+
+
     }
 }
